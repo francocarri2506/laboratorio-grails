6c9963b549cb34db7c3ab1b97a0f661130f3a478
francocarri2506 <carrizonicolasfranco@gmail.com>
2019-10-10 08:09:28 -0300

    restricciones en todos los controller
    recordar que el controler de usuario, expositor,autoridades se deben actualizar y  algun controladores eliminarce... esto en la parte del administrador ya

--- Cursos/grails-app/controllers/cursos/SecurityInterceptor.groovy
+++ Cursos/grails-app/controllers/cursos/SecurityInterceptor.groovy
@@ -18,7 +18,72 @@ class SecurityInterceptor {
       match(controller:"curso", action:"edit")
       match(controller:"curso", action:"show")
       match(controller:"curso", action:"create")
+
+      match(controller:"administrador", action:"index")
+      match(controller:"administrador", action:"edit")
+      match(controller:"administrador", action:"show")
+      match(controller:"administrador", action:"create")
+
       
+      match(controller:"alumno", action:"edit")
+      match(controller:"alumno", action:"show")
+      match(controller:"alumno", action:"create")
+
+      match(controller:"autoridadCertificante", action:"edit")
+      match(controller:"autoridadCertificante", action:"show")
+      match(controller:"autoridadCertificante", action:"create")
+
+      match(controller:"cuponPago", action:"index")
+      match(controller:"cuponPago", action:"edit")
+      match(controller:"cuponPago", action:"show")
+      match(controller:"cuponPago", action:"create")
+
+      match(controller:"docente", action:"edit")
+      match(controller:"docente", action:"show")
+      match(controller:"docente", action:"create")
+
+      match(controller:"expositor", action:"edit")
+      match(controller:"expositor", action:"show")
+      match(controller:"expositor", action:"create")
+
+      match(controller:"formaPago", action:"edit")
+      match(controller:"formaPago", action:"show")
+      match(controller:"formaPago", action:"create")
+
+      match(controller:"horario", action:"edit")
+      match(controller:"horario", action:"show")
+      match(controller:"horario", action:"create")
+
+      match(controller:"inscripcion", action:"edit")
+      match(controller:"inscripcion", action:"show")
+      match(controller:"inscripcion", action:"create")
+
+      match(controller:"interesado", action:"edit")
+      match(controller:"interesado", action:"show")
+      match(controller:"interesado", action:"create")
+
+      match(controller:"login", action:"index")
+      match(controller:"login", action:"edit")
+      match(controller:"login", action:"show")
+      match(controller:"login", action:"create")
+
+      match(controller:"precio", action:"edit")
+      match(controller:"precio", action:"show")
+      match(controller:"precio", action:"create")
+
+      match(controller:"publicoGeneral", action:"edit")
+      match(controller:"publicoGeneral", action:"show")
+      match(controller:"publicoGeneral", action:"create")
+
+      match(controller:"usuario", action:"index")
+      match(controller:"usuario", action:"edit")
+      match(controller:"usuario", action:"show")
+      match(controller:"usuario", action:"create")
+
+      
+
+
+
   }
 
   boolean before() {
