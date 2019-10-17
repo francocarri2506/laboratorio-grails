b741e431a1c9b3f50df7ff040b6e361be3830bc1
francocarri2506 <carrizonicolasfranco@gmail.com>
2019-10-17 14:20:59 -0300

    para que se vea el curso en la lista de cursos

--- Cursos/grails-app/controllers/cursos/SecurityInterceptor.groovy
+++ Cursos/grails-app/controllers/cursos/SecurityInterceptor.groovy
@@ -16,7 +16,7 @@ class SecurityInterceptor {
 
       
       match(controller:"curso", action:"edit")
-      match(controller:"curso", action:"show")
+      
       match(controller:"curso", action:"create")
 
       match(controller:"administrador", action:"index")
@@ -107,10 +107,9 @@ class SecurityInterceptor {
 
   boolean after() { 
 
-     match(controller:"curso", action:"index")
-      match(controller:"curso", action:"edit")
-      match(controller:"curso", action:"show")
-      match(controller:"curso", action:"create")
+//aca habria que poner cada vez que quieramos que vuelva a loquearce  
+//el admin por ejemplo cada vez que quiera modificar algo
+
 
 
       match(controller:"administrador", action:"index")
